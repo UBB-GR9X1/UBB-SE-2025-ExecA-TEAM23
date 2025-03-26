220a9e98828b6f0397a81d2eafa7f336a1a06074
Alexandru Țigănașu <alexandru.tiganasu@stud.ubbcluj.ro>
2025-03-26 18:52:01 +0200

    first iteration for model, added admin, user, doctor, patient, department

---
+++ Hospital/Models/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    class Admin
+    {
+        private int AdminId { get; set; }
+        private int UserId { get; set; }
+
+        public Admin(int adminId, int userId)
+        {
+            AdminId = adminId;
+            UserId = userId;
+        }
+    }
+}
