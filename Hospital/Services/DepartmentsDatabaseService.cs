5d949525b03f376bd73cd35fd32672dfc985d26b
MihneaVoda <mihnea.voda@stud.ubbcluj.ro>
2025-03-29 15:59:59 +0200

    added DepartmentManagerModel and DepartmentsDatabaseService

---
+++ Hospital/Services/DepartmentsDatabaseService.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hospital.Configs;
+using Hospital.Models;
+using Microsoft.Data.SqlClient;
+namespace Hospital.Services
+{
+    public class DepartmentsDatabaseService
+    {
+        private readonly Config _configs;
+
+        public DepartmentsDatabaseService()
+        {
+            _configs = Config.GetInstance();
+        }
+
+        public async Task<List<Department>> GetDepartmentsFromDB()
+        {
+            List<Department> departments = new List<Department>();
+            string connectionString = _configs.DatabaseConnection;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "SELECT * FROM Departments";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            Department department = new Department(
+                                reader.GetInt32(0), 
+                                reader.GetString(1) 
+                            )
+                            {
+                                Description = reader.GetString(2)
+                            };
+                            departments.Add(department);
+                        }
+                    }
+                }
+            }
+            return departments;
+        }
+    }
+}
