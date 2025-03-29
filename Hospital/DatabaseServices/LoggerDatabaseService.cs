dbc240065d2a2060b186f44657c2dd752903d07b
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-03-29 14:01:31 +0200

    Created LogEntry

---
+++ Hospital/DatabaseServices/LoggerDatabaseService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Configs;
+
+namespace Hospital.DatabaseServices
+{
+    public class LoggerDatabaseService
+    {
+        private readonly Config _config;
+
+        public LoggerDatabaseService()
+        {
+            _config = Config.GetInstance();
+        }
+    }
+}
---
+++ Hospital/Models/LogEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    // Open for more actions
+    public enum ActionType
+    {
+        LOGIN,
+        LOGOUT,
+        UPDATE_PROFILE,
+        CHANGE_PASSWORD,
+        DELETE_ACCOUNT
+    }
+
+    public class LogEntry
+    {
+        public int LogId { get; set; }
+        public int UserId { get; set; }
+        public ActionType Action { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public LogEntry(int logId, int userId, ActionType action, DateTime timestamp)
+        {
+            LogId = logId;
+            UserId = userId;
+            Action = action;
+            Timestamp = timestamp;
+        }
+    }
+}
