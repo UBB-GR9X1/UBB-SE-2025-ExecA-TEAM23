26b305406a08417dc9302d4444c58fea689b7a58
Mihai005 <talumihai@gmail.com>
2025-03-30 01:42:37 +0200

    AuthView + Changed MainWindow to show Authentication

--- Hospital/MainWindow.xaml.cs
+++ Hospital/MainWindow.xaml.cs
@@ -12,25 +12,52 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Hospital.DatabaseServices;
+using Hospital.Managers;
+using Hospital.ViewModels;
+using System.Threading.Tasks;
+using Hospital.Exceptions;
+using Hospital.Views;
 
 namespace Hospital
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+
+        private readonly AuthViewModel _viewModel;
+
         public MainWindow()
         {
             this.InitializeComponent();
+
+            LogInDatabaseService logInService = new LogInDatabaseService();
+            AuthManagerModel managerModel = new AuthManagerModel(logInService);
+            _viewModel = new AuthViewModel(managerModel);
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            string username = UsernameField.Text;
+            string password = PasswordField.Password;
+
+            try
+            {
+                bool successful = await _viewModel.Login(username, password);
+            }
+            catch (AuthenticationException ex)
+            {
+                var validationDialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"{ex.Message}",
+                    CloseButtonText = "OK"
+                };
+
+                validationDialog.XamlRoot = this.Content.XamlRoot;
+                await validationDialog.ShowAsync();
+            }
+
+            // implement what happens after successful Login
         }
     }
 }
