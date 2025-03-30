1fc4ef1a4dd1205327bf5273153c6f5d3488c729
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-03-30 11:18:56 +0300

    done LoggerManagerModel

---
+++ Hospital/Managers/LoggerManagerModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.DatabaseServices;
+using Hospital.Models;
+
+namespace Hospital.Managers
+{
+    public class LoggerManagerModel
+    {
+        private readonly LoggerDatabaseService _loggerDatabaseService;
+        private readonly List<LogEntryModel> _logsList;
+
+        public LoggerManagerModel()
+        {
+            _loggerDatabaseService = new LoggerDatabaseService();
+            _logsList = new List<LogEntryModel>();
+        }
+
+        public List<LogEntryModel> LogsList => _logsList;
+
+        public async void LoadAllLogs()
+        {
+            _logsList.Clear();
+            _logsList.AddRange(await _loggerDatabaseService.GetLogsFromDB());
+        }
+
+        public async void LoadLogsByUserId(int userId)
+        {
+            _logsList.Clear();
+            _logsList.AddRange(await _loggerDatabaseService.GetLogsByUserId(userId));
+        }
+
+        public async void LoadLogsByActionType(ActionType actionType)
+        {
+            _logsList.Clear();
+            _logsList.AddRange(await _loggerDatabaseService.GetLogsByActionType(actionType));
+        }
+
+        public async void LoadLogsBeforeTimestamp(DateTime timestamp)
+        {
+            _logsList.Clear();
+            _logsList.AddRange(await _loggerDatabaseService.GetLogsBeforeTimestamp(timestamp));
+        }
+    }
+}
