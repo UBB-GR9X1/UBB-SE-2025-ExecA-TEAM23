3abd5151ff3eccfe683e761011775c6f5356045a
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-03-31 11:38:50 +0300

    Changed DoctorDBService wrong name

--- Hospital/MainWindow.xaml.cs
+++ Hospital/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System;
 using Microsoft.Data.SqlClient;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
