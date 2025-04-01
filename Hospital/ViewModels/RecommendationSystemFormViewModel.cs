b6ab3da4798e1aa6b05d9af63f231d54d1ab1cc6
MihaiTopi <mihai.topor@outlook.com>
2025-04-01 16:08:10 +0300

    Finished recommendation system

--- Hospital/ViewModels/RecommendationSystemFormViewModel.cs
+++ Hospital/ViewModels/RecommendationSystemFormViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
 public class RecommendationSystemFormViewModel : INotifyPropertyChanged
 {
@@ -29,21 +30,21 @@ public class RecommendationSystemFormViewModel : INotifyPropertyChanged
     public string SelectedSymptom1
     {
         get => _selectedSymptom1;
-        set { _selectedSymptom1 = value; OnPropertyChanged(); ValidateSymptoms(); }
+        set { _selectedSymptom1 = value; OnPropertyChanged(); }
     }
 
     private string _selectedSymptom2;
     public string SelectedSymptom2
     {
         get => _selectedSymptom2;
-        set { _selectedSymptom2 = value; OnPropertyChanged(); ValidateSymptoms(); }
+        set { _selectedSymptom2 = value; OnPropertyChanged(); }
     }
 
     private string _selectedSymptom3;
     public string SelectedSymptom3
     {
         get => _selectedSymptom3;
-        set { _selectedSymptom3 = value; OnPropertyChanged(); ValidateSymptoms(); }
+        set { _selectedSymptom3 = value; OnPropertyChanged(); }
     }
 
 
@@ -68,24 +69,31 @@ public class RecommendationSystemFormViewModel : INotifyPropertyChanged
 
         // Here wecan create default values for the fields
         // I opted to use the placeholder text in the XAML file instead since I think it is nicer
-
-        SelectedSymptomStart = SymptomStartOptions[0];
-        SelectedDiscomfortArea = SymptomDiscomfortAreas[0];
-        SelectedSymptom1 = "None";
-        SelectedSymptom2 = "None";
-        SelectedSymptom3 = "None";
-
+        
+        //SelectedSymptomStart = SymptomStartOptions[1];
+        //SelectedDiscomfortArea = SymptomDiscomfortAreas[4];
+        SelectedSymptom1 = SymptomTypes[7];
+        SelectedSymptom2 = SymptomTypes[7];
+        SelectedSymptom3 = SymptomTypes[7];
+        
     }
 
     private void ValidateSymptoms()
     {
-        if (SelectedSymptom1 != "None" && SelectedSymptom2 == SelectedSymptom1)
-            SelectedSymptom2 = "None";
-        if (SelectedSymptom1 != "None" && SelectedSymptom3 == SelectedSymptom1)
-            SelectedSymptom3 = "None";
-        if (SelectedSymptom2 != "None" && SelectedSymptom3 == SelectedSymptom2)
-            SelectedSymptom3 = "None";
+        Debug.WriteLine($"Validating Symptoms: {SelectedSymptom1}, {SelectedSymptom2}, {SelectedSymptom3}");
+
+        // Only perform validation when symptoms are not "None"
+        if (SelectedSymptom1 != SymptomTypes[7] && SelectedSymptom2 == SelectedSymptom1)
+            SelectedSymptom2 = string.Empty; // Or set to "None" based on your logic
+        if (SelectedSymptom1 != SymptomTypes[7] && SelectedSymptom3 == SelectedSymptom1)
+            SelectedSymptom3 = string.Empty; // Or set to "None"
+        if (SelectedSymptom2 != SymptomTypes[7] && SelectedSymptom3 == SelectedSymptom2)
+            SelectedSymptom3 = string.Empty; // Or set to "None"
+
+        Debug.WriteLine($"After Validation: {SelectedSymptom1}, {SelectedSymptom2}, {SelectedSymptom3}");
     }
+
+
 
     private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
